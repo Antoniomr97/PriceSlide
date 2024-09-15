d8abdc3dff3ceab54db6647d9f8b1a1aae64ad55
Antonio Martínez <AntonioMartinezRuiz1997@hotmail.com>
2024-09-15 15:01:48 +0200

    15/09 Fix Search

--- app/routes.py
+++ app/routes.py
@@ -1,39 +1,27 @@
-# Importamos Flask para manejar el acceso de datos de solicitudes y la generación de Urls y diversos usos
 from flask import render_template, redirect, url_for, request
-# Importamos Flask-Login para la gestion de la autentificación y sesiones de usuario
 from flask_login import login_user, logout_user, current_user, login_required
-# Importamos todo lo necesario de app
 from app import app, db, bcrypt, login_manager
 from app.models import User, Game
 from app.forms import LoginForm, RegistrationForm
 from app.eneba_scraper import fetch_game_data_eneba
 from app.instant_gaming_scraper import fetch_game_data_instant_gaming
-# Importando pandas para el análisis de datos
 import pandas as pd
-# Importamos matplotlib para la generación de graficos
 import matplotlib
 import matplotlib.dates as mdates
-# Utilizamos Agg para usar el backend sin interfaz, es util para generar graficos en entornos sin interfaz grafica como un servidor
 matplotlib.use('Agg')
-# Proporciona una interfaz para crear gráficos
 import matplotlib.pyplot as plt
-# Un modulo que proporciona herramientas para trabajar con flujos de entrada y salida(se utiliza para crear graficos)
 import io
 import base64
-# Para realizar busquedas aproximadas
 from fuzzywuzzy import process, fuzz
 
-# Esta ruta carga al usuario cargando su id de la base de datos
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Esta ruta hace que el programa empiece en login
 @app.route('/')
 def index():
     return redirect(url_for('login'))
 
-# Esta ruta realiza el registro hasheando la contraseña y devuelve al login
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -45,7 +33,6 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-# Esta ruta genera el login y confirma que está registrado en la base de datos
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -56,52 +43,38 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', form=form)
 
-# En esta ruta está toda la logica para la llamada del producto
 @app.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
-    # Generamos las variables que vamos a usar
     best_match_game = None
     additional_matches = []
     historical_data = []
     graph_img = None
     game_name = request.args.get('game_name')
 
-    # Comprobamos que hay juego
     if game_name:
-        # Obtienes los juegos
+        # Obtener todos los nombres de juegos en la base de datos
         all_games = Game.query.with_entities(Game.name).distinct().all()
-        # Obtienes los nombres de los juegos
-        game_names = [game.name for game in all_games]
+        game_names = [game.name.lower() for game in all_games]
 
-        # Extrae las mejores coincidencias con extractBests marcandole los parametros
-        matches = process.extractBests(game_name, game_names, limit=5, scorer=fuzz.partial_ratio)
-        # Utilizado para conseguir la coincidencia más parecida
-        best_match = max(matches, key=lambda x: x[1])  # Usar max en lugar de min para obtener la mejor coincidencia
-
-        # Elige el primer elemento de los mejores match
-        best_name = best_match[0]
-
-        # Busca los juegos en la base de datos
-        games_in_db = Game.query.filter(Game.name.ilike(f'%{best_name}%')).all()
-
-        if games_in_db:
-            # Compara los precios de la busqueda y pone el de menor precio
-            best_price_entry = min(games_in_db, key=lambda x: x.price)
-
-            # Registramos el mejor juego
+        # Buscar coincidencia exacta (respetar mayúsculas y minúsculas)
+        exact_match = Game.query.filter(Game.name.ilike(f'%{game_name}%')).all()
+        
+        if exact_match:
+            # Obtener el mejor precio actual
+            best_price_entry = min(exact_match, key=lambda x: x.price)
             best_match_game = {
-                'name': best_name,
+                'name': best_price_entry.name.title(),
                 'best_price': best_price_entry.price,
                 'best_store': best_price_entry.store
             }
 
             # Obtener datos históricos de precios para el mejor juego
-            historical_data = [{
+            historical_data = [ {
                 'date': game.date,
                 'price': game.price,
                 'store': game.store
-            } for game in games_in_db]
+            } for game in exact_match]
 
             # Filtrar datos históricos para mostrar solo las fechas con cambios de precio
             filtered_historical_data = []
@@ -109,7 +82,6 @@ def home():
                 group = group.sort_values(by='date')  # Ordenar por fecha
                 previous_price = None
                 for _, row in group.iterrows():
-                    # Solo guardar la fila si el precio ha cambiado
                     if previous_price is None or row['price'] != previous_price:
                         filtered_historical_data.append(row)
                     previous_price = row['price']
@@ -121,12 +93,11 @@ def home():
 
                 # Agrupar por tienda y trazar los puntos donde hubo cambios de precio
                 for store, group in df.groupby('store'):
-                    plt.plot(group['date'], group['price'], label=store, marker='o')  # Usar marcadores para los puntos de cambio
+                    plt.plot(group['date'], group['price'], label=store, marker='o')
 
-                # Aplicamos los datos que aparecerán en Date y Price
                 plt.xlabel('Date')
                 plt.ylabel('Price')
-                plt.title(f'Price Evolution - {best_name}')
+                plt.title(f'Price Evolution - {best_price_entry.name.title()}')
                 plt.legend()
 
                 # Formatear las fechas correctamente en el eje X
@@ -142,38 +113,36 @@ def home():
                 plt.close()
                 graph_img = base64.b64encode(img.getvalue()).decode('utf-8')
 
-        # Generamos las coincidencias extras
-        additional_matches = []
-        for match in matches:
-            if match[0] != best_name:
+        else:
+            # Buscar coincidencias aproximadas si no hay coincidencia exacta
+            matches = process.extractBests(game_name, game_names, limit=5, scorer=fuzz.partial_ratio)
+            best_matches = [match for match in matches if match[1] >= 80]  # Ajustar el umbral según sea necesario
+
+            for match in best_matches:
                 name = match[0]
                 games_in_db = Game.query.filter(Game.name.ilike(f'%{name}%')).all()
-
                 if games_in_db:
                     best_price_entry = min(games_in_db, key=lambda x: x.price)
                     additional_matches.append({
-                        'name': name,
+                        'name': name.title(),
                         'best_price': best_price_entry.price,
                         'best_store': best_price_entry.store
                     })
 
     return render_template('home.html', best_match_game=best_match_game, additional_matches=additional_matches, graph_img=graph_img)
 
-# Utiliza el logout para cerrar sesion y volver al login 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-# Inicia el scraper en otra pagina del navegador
 @app.route('/update_games_eneba', methods=['POST'])
 @login_required
 def update_games_eneba():
     fetch_game_data_eneba()
     return redirect(url_for('home'))
 
-# Inicia el scraper en otra pagina del navegador
 @app.route('/update_games_instant_gaming', methods=['POST'])
 @login_required
 def update_games_instant_gaming():
